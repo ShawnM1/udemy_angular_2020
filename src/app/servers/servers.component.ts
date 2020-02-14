d7fadcd8539cd8526fce61c027e54da70a78749e
Shawn McCall <bigybonstrangle@yahoo.com>
2020-02-14 10:21:34 -0500

    $event used to get event data

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "no server was created.";
+  serverName = "";
   constructor() { 
     setTimeout(() => {
       this.allowNewServer = true;
@@ -19,6 +20,9 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = "server was created.";
+  }
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
   }
 
 }
