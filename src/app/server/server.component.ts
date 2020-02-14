977032aea65b68cd3a5e28f3e1bca2f7cdcca943
Shawn McCall <bigybonstrangle@yahoo.com>
2020-02-14 09:57:07 -0500

    add string interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
+  serverId: number = 10;
+  serverStatus: String = "offline";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
   }
 
 }
